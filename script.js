fe7ee3e8b1894b7b7c8ef67076b94920dc96798f
Yash Trivedi <yashtrivedi.contact@gmail.com>
2025-11-15 00:12:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,259 @@
+// // // var - global scope //don't use
+// // var anime = "tiger";
+// // var anime = "lion";
+// // console.log(anime); // Output: lion
+// // anime = "bear";
+// // console.log(anime); // Output: bear
+// // // const and let
+// // // let :-
+// // let ageOfStudent = 11;
+// // ageOfStudent = 12;
+// // console.log(ageOfStudent); //no-constant 12
+// // const ki = "sdfsdfas"
+// // console.log(ki); // out s
+// // ki = NaN // err -> 1. string 2. ""
+// // console.log(ki); // out ki
+// // const op = 23;
+// // console.log(op);
+// // 1. var
+// // {
+// //   let name = true;
+// //   console.log(name);
+// // }
+// // console.log(name);
+// // {
+// //   console.log(name);
+// // }
+
+const nodemon = require("nodemon");
+
+// // number
+// // let num = 1.23;
+// // console.log(num);
+// // console.log(typeof num);
+
+// // string:- dcasdcvsdc "sdsdasd" 'dsadfasdf' only in js
+// let vara = "yash";
+// let name = "simran";
+// console.log(vara);
+// console.log(name);
+// // let num = 12;
+// let num = 12.12123123;
+
+// console.log(num);
+// console.log(typeof num);
+
+// bollean true or false
+// let fail = false;
+// fail = true;
+// console.log(fail);
+// console.log(typeof fail);
+
+// undefined
+// let nameOfStudent;
+// console.log(nameOfStudent);
+// console.log(typeof nameOfStudent);
+
+// NULL
+// let ageOfSimran =
+// console.log(ageOfSimran);
+// console.log(typeof ageOfSimran);
+
+// let laptop = {
+//   // key : value
+//   Brand: "HP",
+//   Model: "HP-15",
+// };
+// console.log(laptop.Model);
+// console.log(laptop.Brand);
+// console.log(typeof laptop);
+
+// array:- array is a type of a object
+// let pika = [1, 2, 3, 4, 4, 5];
+// console.log(pika);
+// console.log(typeof pika);
+
+// let a = null;
+// let b = 314311136418273649182723412345124523452453245234523452345234523452345234523452345234523452345234534253345234523452345324532453245346918273641892736418927364918723641829734612873461289374612987346128374612983746129873461293847129873461287346128973461289374618237461892734618923476218934672184761283476129834671289374612983746128937461928374;
+// console.log(a);
+// console.log(typeof a);
+// console.log(b);
+// console.log(typeof b);
+
+// let a = 12;
+// console.log(typeof a);
+
+// case 1
+// let a = true;
+// let b = 1;
+// console.log(a + b);
+
+// // case2
+// console.log(true + 1);
+
+// // case3
+// console.log("vinod" + "thapa");
+
+// test
+// console.log(10 + "20"); //1020
+// console.log(9 - "5"); //err
+// console.log("Java" + "Script"); // JavaScript
+// console.log(" " + " "); //blank
+// console.log(" " + 0); //0
+// console.log(true + true); // truetrue
+// console.log(true + false); //truefalse
+// console.log(false + true); //falsetrue
+
+// let arr1 = [1, 2 ];
+// let arr2 = ["Simran", "Yash"];
+// console.log(arr1 + arr2);
+
+// 1.
+// console.log(10 + "20");
+// console.log(null + "asd");
+
+// let
+
+// console.log(1+1);
+
+// let num1 = parseInt(prompt("Enter a number"));
+// let num2 = parseInt(prompt("Enter second number"));
+
+// let c = num1 + num2;
+// console.log(typeof d);
+// document.writeln("addition is ", num1 + num2);
+// document.writeln("subtraction is ", num1 - num2);
+// document.writeln("subtraction is ", num1 * num2);
+// document.writeln("subtraction is ", num1 / num2);
+
+// +:> add num add
+// let num = [1, 2, 3, 4, 5];
+// let name1 = ["Yash", "Simran"]; // 0 ,1
+// let name2 = ["Yash", "Simran"]; // 0 ,1
+// // console.log(num + name);
+// // name1.push("op")
+// // name1.pop()
+// console.log(name1[1]);
+// console.log(name1.at(1));
+// let c = name1.concat(name2)
+// console.log(c);
+//
+
+// operators and operands
+// let a = 20;
+// let b = "20";
+
+// console.log((a = b)); //assign
+// console.log(a == b); //loose equa //data ki value
+// console.log(a === b); //strict  // data ki value ke sath data ke datat type
+// let a = 1;
+// let b = 2;
+// arith ope
+// console.log(a+b); //1p
+// console.log(a-b); //err
+// console.log(a*b); //
+// console.log(a**b);
+// console.log(a / b);
+// console.log(a % b); //modulus
+// console.log(a++); //1 Post increment
+// console.log(++a); //2 Pre Increment
+// // console.log(a--); //1 PD
+// // console.log(--a); //0 PrD
+
+// assignment operator
+// let a = 1;
+// let b = 2;
+// add and assign
+// let x = 10;
+// x+=5;
+// x = x + 5;
+// console.log(x);
+
+// sub and assign
+// let v = 100;
+// v-=20;
+// v = v-2
+// console.log(v);
+
+// mul and ass
+// let v = 100;
+// v *= 20;
+// console.log(v);
+
+// divide and assign
+// let v = 10;
+// v /= 2;
+// console.log(v);
+
+// modulus and assign
+// let v = 10;
+// v %= 4;
+// console.log(v);
+
+// comp operator
+// 1. == Equality value check only
+// const a = 1;
+// const b = 2;
+// console.log(a == b);
+// {
+//   const a = 1;
+//   const b = 2;
+//   console.log(a);
+// }
+// console.log(b);
+
+// let a= 10;
+// let b= "10";
+// console.log(a===b); // data ki value and data, data type
+
+// let a = 5;
+// let b = 5;
+// console.log(a!=b);
+// console.log(a=b);
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a != b); //loose not equality
+// console.log(a !== b); //strict not equality
+// console.log(a>b);
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+// operator:-
+// 1. Arithmatic // math
+// 2. Assignment // assign
+// 3. Comparison // output true/false
+// 4. Logical // &&:- And      ||:-> OR     ! :-> Not
+// And &&
+// AND (&&)
+// console.log("AND examples:"); // agr all conditions true hongi tabhi apka true aaye ga otherise false aayega
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let e = a === b;
+// let f = c === d;
+// console.log(e && f);
+
+// // OR (||)
+// console.log("\nOR examples:");
+// console.log(10 > 20 || 5 > 2); // true  (one true)
+// console.log(10 > 20 || 5 < 2); // false (both false)
+
+// // NOT (!)
+// console.log("\nNOT examples:");
+// console.log(!true);             // false
+// console.log(!false);            // true
+
+// IN - for objects and arrays
+const user = { name: "Yash", age: 21 };
+let phone = { Model: "M Series" };
+// console.log("name" in user); // true
+// console.log("email" in user); // false
+console.log("Model" in phone);
+
+const arr = [10, 20, 30];
+// console.log(0 in arr); // true (index exists)
+// console.log(5 in arr); // false (index not exists)
+// 1. what is server , localhost and hosting, web servers (Xamp, Apache) and apis testing(Postman or ThunderClient)
+// js:- in and or not comparison
